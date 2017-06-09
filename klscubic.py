478e29645d43e1b44ce271ce55d831421e0db551
kasperkls02 <kasperkls02@gmail.com>
2017-06-09 10:36:58 +0200

    Working

--- klscubic.py
+++ klscubic.py
@@ -4,7 +4,7 @@ import sys,socket
 import math
 import cmath
 from math import sqrt
-from decimal import *
+#from decimal import *
 try: 
     if sys.argv[5] == '-p':
         import matplotlib.pyplot as plt
@@ -24,34 +24,84 @@ except:
 	b = float(raw_input("b = "))
 	c = float(raw_input("c = "))
 	d = float(raw_input("d = "))
-getcontext().prec = 28
-f = Decimal(((3*c/a)-(b**2/a**2))/3)
-g = Decimal(((2*b**3/a**3)-(9*b*c/a**2)+(27*d/a))/27)
-h = Decimal((g**2/4)+(f**3/27))
+#getcontext().prec = 28
+f = ((3*c/a)-(b**2/a**2))/3
+g = ((2*b**3/a**3)-(9*b*c/a**2)+(27*d/a))/27
+h = (g**2/4)+(f**3/27)
 print("f = {0}".format(f))
 print("g = {0}".format(g))
 print("h = {0}".format(h))
 
 if h > 0:
     print("There is only 1 real root.")
+    R = -(g/2)+(h)**(float(1)/2)
+    S = (R)**(float(1)/3)
+    T = -(g/2)-(h)**(float(1)/2)
+    if T > 0:
+        U = math.pow(T, float(1)/3)
+    elif T < 0:
+        U = -math.pow(abs(T), float(1)/3)
+    else:
+        print("Error only 1 real root.")
+    #U = (T)**(float(1)/3)
+    q = ((g**2/4)-h)
+    if q > 0:
+        i = math.pow(q, float(1)/2)
+    elif q < 0:
+        i = -math.pow(abs(q), float(1)/2)
+    else:
+        print("Error Only 1 real root")
+    X_1 = (S+U)-(b/(3*a))
+    X_2_3 = (-(S+U)/2)-(b/(3*a))
+    i_X = ((S-U)*(3)**(float(1)/2))/2
+    
+
+    print("R = {0}".format(R))
+    print("S = {0}".format(S))
+    print("T = {0}".format(T))
+    print("U = {0}".format(U))
+    print("i = {0}".format(i))
+    print("i_X = {0}".format(i_X))
+    print("X1 = {0}".format(X_1))
+    print("X2 = {0} + i*{1}".format(X_2_3, i_X))
+    print("X3 = {0} - i*{1}".format(X_2_3, i_X))
 elif f == 0 and \
      g == 0 and \
      h == 0:
         print("All 3 roots are real and equal.")
+        f = (((3*c)/a)-(b**2/a**2))/3
+        g = ((((2*b**3)/a**3)-((9*b*c))/a**2)+((27*d)/a))/27
+        x = (d/a)**(float(1)/3)*-1
+        print("f = {0}".format(f))
+        print("g = {0}".format(g))
+        print("X1 = X2 = X3 = {0}".format(x))
 else :
     print("All 3 roots are real")
-    q = Decimal(((g**2/4)-h))
+    q = ((g**2/4)-h)
     if q > 0:
-        i = Decimal(math.pow(q, float(1)/2))
+        i = math.pow(q, float(1)/2)
     elif q < 0:
-        i = Decimal(-math.pow(abs(q), float(1)/2))
+        i = -math.pow(abs(q), float(1)/2)
     else:
-        print("Error line 48")
+        print("Error All 3 roots")
 
-    j = Decimal(math.pow(i, float(1)/3))
+    j = math.pow(i, float(1)/3)
 
-    k = math.acos(-(g/(2*i)))
+    K = math.acos(-(g/(2*i)))
+    L = j*-1
+    M = math.cos(K/3)
+    N = math.sqrt(3)*math.sin(K/3)
+    P = (b/(3*a))*-1
+    X_1 = (2*j)*math.cos(K/3)-(b/(3*a))
+    X_2 = L*(M+N)+P
+    X_3 = L*(M-N)+P
     print("i = {0}".format(i))
     print("j = {0}".format(j))
-    print("k = {0}".format(k))
-    
+    print("K = {0}".format(K))
+    print("L = {0}".format(L))
+    print("M = {0}".format(M))
+    print("N = {0}".format(N))
+    print("P = {0}".format(P))
+    print("X1 = {0}".format(X_1))
+    print("X2 = {0}".format(X_2))
+    print("X3 = {0}".format(X_3))
